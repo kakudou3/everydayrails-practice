d5c0bde7d2c77dbe79dfbf98220152817e8bfca4
kakudooo <kakudou3@gmail.com>
2022-07-26 09:54:58 +0900

    feat: 4章

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,81 +1,71 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-
-  # ユーザー単位では重複したプロジェクト名を許可しないこと
   it "does not allow duplicate project names per user" do
     user = User.create(
       first_name: "Joe",
       last_name:  "Tester",
       email:      "joetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze"
-    )
+      password:   "dottle-nouveau-pavilion-tights-furze",
+      )
+
     user.projects.create(
-      name: "Test Project"
-    )
+      name: "Test Project",
+      )
+
     new_project = user.projects.build(
-      name: "Test Project"
-    )
+      name: "Test Project",
+      )
+
     new_project.valid?
     expect(new_project.errors[:name]).to include("has already been taken")
   end
 
-  # 二人のユーザーが同じ名前を使うことは許可すること
   it "allows two users to share a project name" do
     user = User.create(
       first_name: "Joe",
-      last_name: "Tester",
-      email: "joetester@example.com",
-      password: "dottle-nouveau-pavilion-tights-furze"
-    )
+      last_name:  "Tester",
+      email:      "joetester@example.com",
+      password:   "dottle-nouveau-pavilion-tights-furze",
+      )
 
     user.projects.create(
-      name: "Test Project"
-    )
+      name: "Test Project",
+      )
 
     other_user = User.create(
       first_name: "Jane",
-      last_name: "Tester",
-      email: "janetester@example.com",
-      password: "dottle-nouveau-pavilion-tights-furze"
-    )
+      last_name:  "Tester",
+      email:      "janetester@example.com",
+      password:   "dottle-nouveau-pavilion-tights-furze",
+      )
 
     other_project = other_user.projects.build(
-      name: "Test Project"
-    )
+      name: "Test Project",
+      )
 
     expect(other_project).to be_valid
   end
 
-  # プロジェクトが遅延している場合は真が返ること
-  it "returns true when project after due_on" do
-    user = User.create(
-      first_name: "Joe",
-      last_name:  "Tester",
-      email:      "joetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze"
-    )
-    project = user.projects.create(
-      name: "Test Project",
-      due_on: Time.zone.yesterday
-    )
+  describe "late status" do
+    it "is late when the due date is past today" do
+      project = FactoryBot.create(:project, :due_yesterday)
+      expect(project).to be_late
+    end
 
-    expect(project.late?).to be_truthy
+    it "is on time when the due date is today" do
+      project = FactoryBot.create(:project, :due_today)
+      expect(project).to_not be_late
+    end
+
+    it "is on time when the due date is in the future" do
+      project = FactoryBot.create(:project, :due_tomorrow)
+      expect(project).to_not be_late
+    end
   end
 
-  # プロジェクトが遅延していない場合は偽が返ること
-  it "returns false when project before due_on" do
-    user = User.create(
-      first_name: "Joe",
-      last_name:  "Tester",
-      email:      "joetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze"
-    )
-    project = user.projects.create(
-      name: "Test Project",
-      due_on: Time.zone.tomorrow
-    )
-
-    expect(project.late?).to be_falsey
+  it "can have many notes" do
+    project = FactoryBot.create(:project, :with_notes)
+    expect(project.notes.length).to eq 5
   end
 end
